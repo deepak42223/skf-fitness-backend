e0b82f4719bbf6450391338c638c48b47cd1c440
Deepak Gome <deepakgome07@example.com>
2026-09-10 12:42:39 +0530

    fix: allow all netlify subdomains in CORS

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:4200',
       'https://skf-fitness.netlify.app',
+      /\.netlify\.app$/,   // sab netlify subdomains allow
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
